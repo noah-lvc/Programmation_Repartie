51be5384e595bf29fc3d4a8284a1b9430c4e9ee5
noah-lvc <lavocatnoah@gmail.com>
2025-09-22 16:26:14 +0200

    Exo 1 | Ajout des fichiers du TP

---
+++ TP2-Section_Critique/src/Affichage.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+import java.io.*;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.lang.String;
+
+class Exclusion{};
+public class Affichage extends Thread{
+	String texte; 
+	static Exclusion exclusionImpression = new Exclusion();
+
+	public Affichage (String txt){texte=txt;}
+	
+	public void run(){
+		//section critique
+		synchronized(exclusionImpression){
+			for (int i=0; i<texte.length(); i++){
+		    	System.out.print(texte.charAt(i)); // ressource critique
+		    	try {sleep(100);} catch(InterruptedException e){};
+			}
+		}
+		//fin section critique
+	}
+}
