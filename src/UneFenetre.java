87a4281a4e92bee5cd9a37b0ce184fb4674f3734
noah-lvc <lavocatnoah@gmail.com>
2025-09-22 14:59:53 +0200

    Exo 3 | Ajout d'un deuxième mobile qui se déplace de gauche à droite puis droite à gauche

--- src/UneFenetre.java
+++ src/UneFenetre.java
@@ -2,16 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class UneFenetre extends JFrame {
-    private UnMobile sonMobile;
-    private final int LARG=400, HAUT=250;
+    private UnMobile sonMobile1;
+    private UnMobile sonMobile2;
+    private final int LARG=400, HAUT=250 , NBRLIG=2, NBRCOL=1;
 
     public UneFenetre() {
         setVisible(true);
         setSize(LARG, HAUT);
-        Container container = getContentPane();
-        sonMobile = new UnMobile(LARG, HAUT);
-        container.add(sonMobile);
-        Thread laTache = new Thread(sonMobile);
-        laTache.start();
+        Container leConteneur = getContentPane(); 
+        leConteneur.setLayout (new GridLayout(NBRLIG, NBRCOL)); 
+
+        sonMobile1 = new UnMobile(LARG, HAUT/NBRLIG);
+        leConteneur.add(sonMobile1);
+        sonMobile2 = new UnMobile(LARG, HAUT/NBRLIG);
+        leConteneur.add(sonMobile2);
+        Thread laTache1 = new Thread(sonMobile1);
+        Thread laTache2 = new Thread(sonMobile2);
+        laTache1.start();
+        laTache2.start();
     }
 }
