1d8567437cabb87616b261b963e347fdd70569bd
noah-lvc <lavocatnoah@gmail.com>
2025-09-29 15:23:55 +0200

    Modification des noms de Main.java de chaque TP

--- TP2-2/src/Affichage.java
+++ TP2-2/src/Affichage.java
@@ -9,16 +9,18 @@ import java.lang.String;
 
 public class Affichage extends Thread{
 	String texte; 
-	static semaphoreBinaire semaphoreBinaire = new semaphoreBinaire(1);
+	static semaphoreBinaire semaphore = new semaphoreBinaire(1);
 
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
 		//debut section critique
+		semaphore.syncWait();
 		for (int i=0; i<texte.length(); i++){
 		    System.out.print(texte.charAt(i));
 		    try {sleep(100);} catch(InterruptedException e){};
 		}
+		semaphore.syncSignal();
 		//fin section critique
 	}
 }
--- TP2-2/src/MainTP22.java
+++ TP2-2/src/MainTP22.java
@@ -4,7 +4,7 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
-public class Main {
+public class MainTP22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
