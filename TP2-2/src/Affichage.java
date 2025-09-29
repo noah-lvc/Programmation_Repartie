2706270fcae00975a8ced70e85c1b1603c81aa56
noah-lvc <lavocatnoah@gmail.com>
2025-09-29 14:56:32 +0200

    Reorganisation des fichiers

--- TP2-2/src/Affichage.java
+++ TP2-2/src/Affichage.java
@@ -9,15 +9,16 @@ import java.lang.String;
 
 public class Affichage extends Thread{
 	String texte; 
+	static semaphoreBinaire semaphoreBinaire = new semaphoreBinaire(1);
 
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
-
+		//debut section critique
 		for (int i=0; i<texte.length(); i++){
 		    System.out.print(texte.charAt(i));
 		    try {sleep(100);} catch(InterruptedException e){};
 		}
-
+		//fin section critique
 	}
 }
