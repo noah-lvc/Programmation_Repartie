70e193df79b05abf7345bb225def52557664b3ab
noah-lvc <162138723+noah-lvc@users.noreply.github.com>
2025-10-13 11:44:32 +0200

    Update semaphoreBinaire.java

--- TP3-Les_Mobiles_Semaphore/src/semaphoreBinaire.java
+++ TP3-Les_Mobiles_Semaphore/src/semaphoreBinaire.java
@@ -4,6 +4,21 @@ public semaphoreBinaire(int valeurInitiale){
 	super((valeurInitiale != 0) ? 1:0);
 	//System.out.print(valeurInitiale);
 }
+	
+public final synchronized void syncSignal(){
+	super.syncSignal();
+	//System.out.print(valeur);
+	if (valeur>1) valeur = 1;
+}
+}
+
+
+public final class semaphoreGeneral extends semaphore {
+public semaphoreGeneral(int valeurInitiale, int n){
+	super((valeurInitiale != 0) ? 1:n);
+	//System.out.print(valeurInitiale);
+}
+	
 public final synchronized void syncSignal(){
 	super.syncSignal();
 	//System.out.print(valeur);
